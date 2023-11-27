4c5ffac558616bb4f14f46e1948380ca9c9ed4c9
lilkoorii <89746507+lilkoorii@users.noreply.github.com>
2023-11-27 18:22:47 +0300

    27/11 necessary files update
    no load more button
    better grid
    linkedin link

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", function () {
   filterButtons.forEach((button) => {
     button.addEventListener("click", () => {
       const filterValue = button.getAttribute("data-filter");
-      filterButtons.forEach((btn) => btn.classList.remove("active")); //убираем у всех
-      button.classList.add("active"); //добавляем к нужной (не путать button и btn)
+      filterButtons.forEach((btn) => btn.classList.remove("active"));
+      button.classList.add("active");
       updateDisplayedItems(filterValue);
     });
   });
@@ -30,12 +30,12 @@ document.addEventListener("DOMContentLoaded", function () {
       link: "https://youtu.be/8J2VWYr6iuw",
       categories: ["all", "3D", "code"]
     },
-    /*{
-      src: "img\\pic-1.png",
-      link: "https://example.com/page2",
-      categories: ["all", "code"]
-    },
     {
+      src: "img\\myfirstcharacter.jpg",
+      link: "https://www.artstation.com/artwork/QXP5dx",
+      categories: ["all", "3D"]
+    },
+    /*{
       src: "img\\small table.png",
       link: "https://example.com/page3",
       categories: ["all", "3D"]
@@ -44,34 +44,23 @@ document.addEventListener("DOMContentLoaded", function () {
   ];
 
   const projectsPictures = document.querySelector(".projects_pictures");
-  const loadMoreButton = document.querySelector(".projects_load");
-  let imagesLoaded = 0;
-  let imagesToShow = 3;
 
-  loadMoreButton.addEventListener("click", () => {
-    for (let i = imagesLoaded; i < imagesLoaded + imagesToShow; i++) {
-      if (i >= imageArray.length) {
-        loadMoreButton.style.display = "none";
-        break;
-      }
+  imageArray.forEach((image) => {
+    const imageContainer = document.createElement("div");
+    imageContainer.classList.add("projects_show");
+    imageContainer.classList.add(...image.categories);
 
-      const imageContainer = document.createElement("div");
-      imageContainer.classList.add("projects_show");
-      imageContainer.classList.add(...imageArray[i].categories);
+    const linkElement = document.createElement("a");
+    linkElement.href = image.link;
 
-      const linkElement = document.createElement("a");
-      linkElement.href = imageArray[i].link;
+    const imgElement = document.createElement("img");
+    imgElement.src = image.src;
+    imgElement.alt = "Project Image";
 
-      const imgElement = document.createElement("img");
-      imgElement.src = imageArray[i].src;
-      imgElement.alt = "Project Image";
-
-      linkElement.appendChild(imgElement);
-      imageContainer.appendChild(linkElement);
-      projectsPictures.appendChild(imageContainer);
-    }
-
-    imagesLoaded += imagesToShow;
-    updateDisplayedItems(document.querySelector(".project_nav_point.active").getAttribute("data-filter"));
+    linkElement.appendChild(imgElement);
+    imageContainer.appendChild(linkElement);
+    projectsPictures.appendChild(imageContainer);
   });
+
+  updateDisplayedItems(document.querySelector(".project_nav_point.active").getAttribute("data-filter"));
 });
